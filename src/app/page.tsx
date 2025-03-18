5671e544e2377a0d9d4bc70b295debe34dd24b76
Chris Klemz <crklemz@gmail.com>
2025-03-18 11:40:56 -0500

    style: mobile padding home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center bg-[var(--background)] text-[var(--foreground)] px-6 relative">
+    <div className="flex flex-col items-center justify-center min-h-[80vh] text-center bg-[var(--background)] text-[var(--foreground)] px-4 md:px-6 lg:px-8 relative">
       <div className={styles.bgAccentEffect}></div>
       < Hero />
       <TechStack />
