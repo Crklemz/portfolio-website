1577d8fe29b95c55eecd4b4952fc5e9a5d3279d7
Chris Klemz <crklemz@gmail.com>
2025-03-18 13:46:26 -0500

    style: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,28 @@
+"use client";
+
+import styles from "@/styles/Footer.module.css";
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 export default function Footer() {
-    return (
-      <footer className="bg-[var(--background)] text-[var(--foreground)] p-4 mt-10 text-center">
-        <p>&copy; {new Date().getFullYear()} Chris Klemz. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerLinks}>
+        <Link href="/projects">Projects</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+
+      <div className={styles.socialIcons}>
+        <a href="https://github.com/Crklemz" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://linkedin.com/in/chrisklemz" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+      </div>
+
+      <p>&copy; {new Date().getFullYear()} Chris Klemz. All rights reserved.</p>
+    </footer>
+  );
+}
