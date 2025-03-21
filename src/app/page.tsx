0cd289e0d63e2e3e870d1cdbd3c4e4e0dac68ce8
Chris Klemz <crklemz@gmail.com>
2025-03-21 11:01:54 -0500

    style: add divider to page and adjust hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,19 +10,17 @@ import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="w-full section-divider"> {/* Ensure full width and proper alignment */}
-      <div className="flex flex-col items-center justify-center min-h-[80vh] text-center bg-[var(--background)] text-[var(--foreground)] px-4 md:px-6 lg:px-8 relative">
-        <div className={styles.bgAccentEffect}></div>
-        <Hero />
-        <SectionDivider />
-        <TechStack />
-        <SectionDivider />
-        <ProjectsSection />
-        <SectionDivider />
-        <Testimonials />
-        <SectionDivider />
-        <ContactSection />
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-[80vh] text-center bg-[var(--background)] text-[var(--foreground)] px-4 md:px-6 lg:px-8 relative">
+      <div className={styles.bgAccentEffect}></div>
+      <Hero />
+      <SectionDivider />
+      <TechStack />
+      <SectionDivider />
+      <ProjectsSection />
+      <SectionDivider />
+      <Testimonials />
+      <SectionDivider />
+      <ContactSection />
     </div>
   );
 }
