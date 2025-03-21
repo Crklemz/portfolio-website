1160913a2881396dfee72b7743fa397be8c1e923
Chris Klemz <crklemz@gmail.com>
2025-03-21 10:49:25 -0500

    feat: create divider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,24 @@ import TechStack from "@/components/TechStack";
 import ProjectsSection from "@/components/ProjectSection";
 import Testimonials from "@/components/Testimonials";
 import ContactSection from "@/components/ContactSection";
+import SectionDivider from "@/components/SectionDivider";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[80vh] text-center bg-[var(--background)] text-[var(--foreground)] px-4 md:px-6 lg:px-8 relative">
-      <div className={styles.bgAccentEffect}></div>
-      < Hero />
-      <TechStack />
-      <ProjectsSection />
-      <Testimonials />
-      <ContactSection />
+    <div className="w-full section-divider"> {/* Ensure full width and proper alignment */}
+      <div className="flex flex-col items-center justify-center min-h-[80vh] text-center bg-[var(--background)] text-[var(--foreground)] px-4 md:px-6 lg:px-8 relative">
+        <div className={styles.bgAccentEffect}></div>
+        <Hero />
+        <SectionDivider />
+        <TechStack />
+        <SectionDivider />
+        <ProjectsSection />
+        <SectionDivider />
+        <Testimonials />
+        <SectionDivider />
+        <ContactSection />
+      </div>
     </div>
   );
 }
