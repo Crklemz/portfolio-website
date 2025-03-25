4d45893bfd0a9d8e23859636f7718a8d3208dc63
Chris Klemz <crklemz@gmail.com>
2025-03-25 14:36:04 -0500

    refactor: remove section divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import HomeTechStack from "@/components/HomeTechStack";
 import ProjectsSection from "@/components/ProjectSection";
 import Testimonials from "@/components/Testimonials";
 import ContactSection from "@/components/ContactSection";
-import SectionDivider from "@/components/SectionDivider";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
@@ -13,13 +12,9 @@ export default function Home() {
     <div className={styles.homeContainer}>
       <div className={styles.bgAccentEffect}></div>
       <Hero />
-      <SectionDivider />
       <HomeTechStack />
-      <SectionDivider />
       <ProjectsSection />
-      <SectionDivider />
       <Testimonials />
-      <SectionDivider />
       <ContactSection />
     </div>
   );
