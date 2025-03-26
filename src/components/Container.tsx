0b969630d817bf0a618d188182e60d2c795eb9cd
Chris Klemz <crklemz@gmail.com>
2025-03-26 11:45:42 -0500

    style: stticky footer

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,7 +4,7 @@ interface ContainerProps {
 
 export default function Container({ children }: ContainerProps) {
     return (
-        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-32">
             {children}
         </div>
     );
